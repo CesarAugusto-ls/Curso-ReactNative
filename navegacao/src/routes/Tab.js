48e910a9677ba7697cb86804a25f0932b3240031
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-08-08 14:12:25 -0300

    tab icons

--- navegacao/src/routes/Tab.js
+++ navegacao/src/routes/Tab.js
@@ -4,32 +4,56 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
 import TelaC from '../views/TelaC';
-import TelaD from '../views/TelaD';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator()
 
 export default props => {
     return (
-        <Tab.Navigator tabBarOptions={{
-            activeTintColor: 'red',
-            inactiveTintColor: 'blue',
-            labelStyle: { fontSize: 30 },
-        }}>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    switch (route.name) {
+                        case 'TelaA':
+                            iconName = focused
+                                ? 'ios-add'
+                                : 'ios-add-circle';
+                            break;
+                        case 'TelaB':
+                            iconName = focused
+                                ? 'ios-add'
+                                : 'ios-add-circle';
+                            break;
+                        case 'TelaC':
+                            iconName = focused ? 'ios-alarm' : 'ios-alarm-outline';
+                            break;
+                        default:
+                            break;
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'blue',
+                inactiveTintColor: 'gray',
+            }}>
             <Tab.Screen
                 name="TelaA"
                 component={TelaA}
+                options={{ title: "Inicial" }}
             />
             <Tab.Screen
                 name="TelaB"
                 component={TelaB}
+                options={{ title: "Dados" }}
             />
             <Tab.Screen
                 name="TelaC"
                 component={TelaC}
-            />
-            <Tab.Screen
-                name="TelaD"
-                component={TelaD}
+                options={{ title: "Config" }}
             />
         </Tab.Navigator>
     )
